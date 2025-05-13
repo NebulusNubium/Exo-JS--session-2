1ead07a7ab4e3691be15454f6eb298beec5cc4d1
Théo <t.knopfer@gmail.com>
2025-05-13 11:05:13 +0200

    exo2 test2

--- Exo2.js
+++ Exo2.js
@@ -2,6 +2,10 @@ function spinalCase(str) {
     return str
       .replace(/([a-z])([A-Z])/g, "$1-$2")
       .replace(/[_\s]+/g, "-")
-      .toLowerCase();       
+      .toLowerCase();                       
   }
-  
+
+console.log(spinalCase("Bonjour Le Monde"));
+console.log(spinalCase("grand_Gaillard"));
+console.log(spinalCase("Petit coeurEn_Sucre"));
+console.log(spinalCase("monSuperTexte"));
