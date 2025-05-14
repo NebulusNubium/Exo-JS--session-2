3a5ccab6427cfdea8e6d7da8e79917ea6be0e159
Théo <t.knopfer@gmail.com>
2025-05-14 11:26:00 +0200

    exo interval

--- date/exo/date.js
+++ date/exo/date.js
@@ -1,5 +1,5 @@
 //Exo 1
-function message(){
+/*function message(){
     const message = 'welcome to JS!'
     const container = document.getElementById('welcome')
     container.textContent = message
@@ -10,3 +10,42 @@ setTimeout(function(){
 }, 2000)
 
 //exo 2
+let seconds = 5
+
+const container = document.getElementById('countdown')
+container.textContent = "Virus injecté dans..."+seconds
+
+const timer = setInterval(() => {
+  seconds--
+
+  if (seconds > 0) {
+    container.textContent = "Virus injecté dans..."+seconds
+  } else {
+    container.textContent = "Virus injecté!"
+    clearInterval(timer)
+  }
+}, 1000)
+*/
+//exo 3
+
+let seconds = 10
+
+const container = document.getElementById('countdown')
+container.textContent = "Virus injecté dans..."+seconds/2
+
+const timer = setInterval(() => {
+  seconds--
+
+  if (seconds%2 == 1) {
+    container.textContent = "Virus injecté dans..."+(seconds+1)/2
+  } else {
+    container.textContent = ""
+  }
+  if (seconds == 0) {
+    container.textContent = "Virus injecté !"
+    clearInterval(timer)
+  }
+}, 500)
+
+
+//Exo 4
